f4e17ea6e8abab7a45c5afc6aa9e1c9ead5bc266
RAWANE18 <rawanezineb689@gmail.com>
2024-03-31 18:25:16 +0100

    second commit

--- src/App.java
+++ src/App.java
@@ -4,7 +4,15 @@ import java.sql.SQLDataException;
 import java.sql.Statement;*/
 
 
+
 public class App {
-  
-	    
-    }
+  /*  static final String URL ="jdbc:mysql://3306/";
+   static final String USER="root";
+   */
+   
+        public static void main(String[] args){
+    
+            System.err.println("hello wolrd");
+	        }
+	    }
+    
