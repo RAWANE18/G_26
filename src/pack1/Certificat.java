46e41f2565f660606f1c85c0a84375c0a576cab9
RAWANE18 <rawanezineb689@gmail.com>
2024-04-02 22:12:40 +0100

    create json file

--- src/pack1/Certificat.java
+++ src/pack1/Certificat.java
@@ -1,20 +1,24 @@
 package pack1;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.time.LocalDate;
+import java.util.Calendar;
 public class Certificat {
     	//private int idCertificat;
     private String contenu;
     private Date dateDelivrance;
     private Date dateExpiration;
     private int idPatient;
-
+    
     public Certificat(String contenu, Date dateDelivrance, Date dateExpiration,int idPatient) {
         this.contenu = contenu;
         this.dateDelivrance = dateDelivrance;
@@ -23,43 +27,57 @@ public class Certificat {
     }
     //Enregistre une certificat du patient en base de données. 
     
-	public void sauvegarderCertificat(Connection connection) throws SQLException {
-        String sql = "INSERT INTO certificats ( date_delivrance, date_expiration, id_patient) VALUES ( ?, ?, ?)";
+	public void sauvegarderCertificat(Connection connection ,String contenu,String last_name) throws SQLException {
+
+        String sql2="SELECT id_patient FROM patient WHERE last_name =?";
+        PreparedStatement preparedStatement2 = connection.prepareStatement(sql2);
+        preparedStatement2.setString(1,last_name );
+        ResultSet resultSet = preparedStatement2.executeQuery();
+        idPatient = resultSet.getInt("id_patient");
+
+        String sql = "INSERT INTO certificats (contenu, date_delivrance, date_expiration, id_patient) VALUES ( ?,?, ?, ?)";
+        java.sql.Date datedelivrance=new java.sql.Date(dateDelivrance.getTime());
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(datedelivrance);
+
+        // Adding 3 days to the current date
+        calendar.add(Calendar.DAY_OF_MONTH, 3);
+
+        // Converting back to java.sql.Date
+        java.sql.Date datexpiration = new java.sql.Date(calendar.getTimeInMillis());
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
-      //  preparedStatement.setString(1, contenu);
-        preparedStatement.setDate(1, new java.sql.Date(dateDelivrance.getTime()));
-        preparedStatement.setDate(2, new java.sql.Date(dateExpiration.getTime()));
-        preparedStatement.setInt(3, idPatient);
+
+        preparedStatement.setString(1, contenu);
+        preparedStatement.setDate(2, datedelivrance);
+        
+        preparedStatement.setDate(3,datexpiration );
+        preparedStatement.setInt(4, idPatient);
+        
         preparedStatement.executeUpdate();
+        ObjectMapper objectmapper = new ObjectMapper();
+        String jsonData= objectMapper.writeValueAsString(new Certificat(contenu, dateDelivrance,dateExpiration, idPatient));
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+    ObjectNode jsonData = objectMapper.createObjectNode();
+    jsonData.put("contenu", contenu);
+    jsonData.put("date_delivrance", new java.sql.Date(dateDelivrance.getTime()).toString());
+    jsonData.put("date_expiration", new java.sql.Date(dateExpiration.getTime()).toString());
+    jsonData.put("id_patient", idPatient);
+
+    File file = new File("src/certificat.json");
+    if (file.exists()) {
+        // Append data to the existing JSON file
+        JsonNode existingData = objectMapper.readTree(file);
+        ((ObjectNode) existingData).put("new_data", jsonData);
+        objectMapper.writeValue(file, existingData);
+    } else {
+        // Write data to a new JSON file
+        objectMapper.writeValue(file, jsonData);
     }
-	public void ajouterCertificat(String contenu,String delivrancedate,String expirationdate,int idCertificat,String Plastname,String Pfirstname) {
-	        String nomFichier = "Certificat" + idCertificat + ".txt";
-            String lastname ="Last Name: "+Plastname;
-            String firstname="First Name: "+Pfirstname;
-            String Date="delivrance date: "+delivrancedate;
-            String EXP="expiration date: "+expirationdate;
-            String style="------------------------------------------------------------------------------";
-	        File file = new File(nomFichier);
-	        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
-                writer.write(style);
-                writer.newLine();
-                writer.write(Date);
-                writer.newLine();
-                writer.write(EXP);
-                writer.newLine();
-                writer.write(lastname);
-                writer.newLine();
-                writer.write(firstname);
-                writer.newLine();
-                writer.write(style);
-                writer.newLine();
-                writer.write(contenu);
-                writer.newLine();
-	           
-	        } catch (IOException e) {
-	            System.err.println("erreur : " + e.getMessage());
-	        }
-	    }
+    fileWriter.close(); 
+
+    }
+	
      //getteur et setteur
 	
 	public String getContenu() {
