41121cddac61cb3a50fcc4691f2dd920e5a064e8
RAWANE18 <rawanezineb689@gmail.com>
2024-04-02 17:20:59 +0100

    third commit

--- src/pack1/Patient.java
+++ src/pack1/Patient.java
@@ -13,86 +13,77 @@ public class Patient extends Personne{
 	private Date Birthdate;
 	private Secretaire sectretaire;
 	private Medcin medcin;
-	private List<String> antecedents ;
-public Patient(String firstname, String lastname, String phonenumber,String email,String gender,String adress,Date Birthdate,int idPatient){
+	
+
+	public Patient(String firstname, String lastname, String phonenumber,String email,String gender,String adress,Date Birthdate,int idPatient)
+	{
 	super(firstname,lastname,phonenumber);
 	this.email=email;
 	this.gender=gender;
     this.Birthdate=Birthdate;
     this.idPatient=idPatient;
-}
+	}
 
 //getteur setteur
-	public int getIdPatient() {
+	public int getIdPatient()
+	{
 		return idPatient;
 	}
 
-	public void setIdPatient(int idPatient) {
+	public void setIdPatient(int idPatient) 
+	{
 		this.idPatient = idPatient;
 	}
-	public String getEmail() {
+	public String getEmail() 
+	{
 		return email;
 	}
 
-	public void setEmail(String email) {
+	public void setEmail(String email) 
+	{
 		this.email = email;
 	}
 
-	public String getGender() {
+	public String getGender() 
+	{
 		return gender;
 	}
 
-	public void setGender(String gender) {
+	public void setGender(String gender) 
+	{
 		this.gender = gender;
 	}
 
-	public String getAdress() {
+	public String getAdress() 
+	{
 		return adress;
 	}
 
-	public void setAdress(String adress) {
+	public void setAdress(String adress) 
+	{
 		this.adress = adress;
 	}
 
-	public Date getBirthdate() {
+	public Date getBirthdate() 
+	{
 		return Birthdate;
 	}
 
-	public void setBirthdate(Date birthdate) {
+	public void setBirthdate(Date birthdate) 
+	{
 		Birthdate = birthdate;
 	}
-
-	// Renvoie les antécédents médicaux du patient
-	public List<String> getAntecedents() {
-        return antecedents;
-    }
 	
-	  //Ajoute un nouvel antécédent médical au patient. 
-	public void ajouterAntecedentMedical(String antecedent) {
-        suiviMedical.ajouterAntecedent(antecedent, idPatient);
-    }
-	
-	//Charge les antécédents médicaux du patient depuis le fichier txt 
-	
-	  public List<String> chargerAntecedentsMedicaux(String nomFichier) throws IOException {
-	        List<String> antecedents = new ArrayList<>();
-	        
-	        try (BufferedReader reader = new BufferedReader(new FileReader(nomFichier))) {
-	            String ligne;
-	            while ((ligne = reader.readLine()) != null) {
-	                antecedents.add(ligne);
-	            }
-	        }
-	        
-	        return antecedents;
-	    }
-	  
-	public void demanderCertificat()
+	  public void demanderCertificat()
 	{    
 		medcin.creationcertificat();
 		
 	}
-	
+	public void demandeRDV()
+	{	
+		sectretaire.ajouterunRDV();
+		
+	}
 	public void demandesupressionRDV()
 	{	
 		sectretaire.supressionRDV();
--- src/pack1/SuiviMedical.java
+++ src/pack1/SuiviMedical.java
@@ -1,60 +1,31 @@
 package pack1;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class SuiviMedical {
-     private List<FichePatient> fichesPatients;
+	 
+	 private FichePatient fichePatient;
 	 private List<Consultation> consultations;
-	 private List<Certificat> certificats;
-	    
-	   
-
-		public SuiviMedical() {
-	        this.fichesPatients = new ArrayList<>();
-	        this.consultations = new ArrayList<>();
-	        this.certificats = new ArrayList<>();
+	 
+		public SuiviMedical(FichePatient fichePatient, List<Consultation> consultations) {
+			this.fichePatient = fichePatient;
+			this.consultations = consultations;
 	        
 	    }
-		//Ajouter une certificat au suivi médica.
-		public void ajouterFichePatient(FichePatient fichePatient) {
-	        fichesPatients.add(fichePatient);
-	    }
+		//getteur
+		public FichePatient getFichePatient() {
+			return fichePatient;
+		}
+	
+		public List<Consultation> getConsultations() {
+			return consultations;
+		}
+		
 		//Ajouter une consultation au suivi médical.
 	    public void ajouterConsultation(Consultation consultation) {
 	        consultations.add(consultation);
 	    }
-	    //Ajouter une certificat au suivi médical.
-	    public void ajouterCertificat(Certificat certificat) {
-	        certificats.add(certificat);
-	    }
 	    
-	    // Création d'un fichier qui contien les antécédents d'un patient .
-	    public void ajouterAntecedent(String antecedent,int idPatient) {
-	        String nomFichier = "patient" + idPatient + ".txt";
-	        File file = new File(nomFichier);
-	        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
-	            writer.write(antecedent);
-	            writer.newLine();
-	           
-	        } catch (IOException e) {
-	            System.err.println("erreur : " + e.getMessage());
-	        }
-	    }
-	    
-	    public List<Certificat> getCertificats() {
-	        return certificats;
-	    }
-	    public List<FichePatient> getFichesPatients() {
-			return fichesPatients;
-		}
-		public List<Consultation> getConsultations() {
-			return consultations;
-		}
 
 		
 }
