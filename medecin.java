9299cd4e67544020a6a09e371f2b6ae97a8b18cf
imeneabbas <142441033+imeneabbas@users.noreply.github.com>
2024-04-05 17:56:09 +0100

    Update medecin.java

--- medecin.java
+++ medecin.java
@@ -9,6 +9,9 @@ import java.sql.Statement;
 public class medecin extends Personne{
 	private String specialite ;
     private patient patient;
+    private Consultation consultation;
+
+
 	
 
 	//constructeur
@@ -31,16 +34,35 @@ public class medecin extends Personne{
         }
         return specialite;
     }
+     //ajouter un medecin 
+     public void ajouterMedecin(String nom,String prenom,String numTel,String specialite){
+        String sql = "INSERT INTO medecin(nom,prenom,numTel,specialite) VALUES (?, ?, ?, ?)";
+        PreparedStatement pstmt = con.prepareStatement(sql);
+        pstmt.setString(1,nom);
+        pstmt.setString(2,prenom);
+        pstmt.setInt(3,numTel);
+        pstmt.setString(4,specialite);
+
+     }
 
     //ajouter une ordonnace
-    public void ajouterOrdonnance(Patient patient) throws SQLException {
-        String sql = "INSERT INTO ordonnance(id_patient, date_ordonnance, ordonnance) VALUES (?, ?, ?)";
-        PreparedStatement pstmt = con.prepareStatement(sql);
-        pstmt.setInt(1, patient.getId()); 
-        pstmt.setString(2, patient.getDateOrdonnance()); 
-        pstmt.setString(3, patient.getOrdonnance());
+    public void ajouterOrdonnance(int id_patient,String date_ordonnance,String ordonnace) throws SQLException {
+        String sql1 = "INSERT INTO ordonnance(id_patient, date_ordonnance, ordonnance) VALUES (?, ?, ?)";
+        PreparedStatement pstmt1 = con.prepareStatement(sql1);
+        pstmt1.setInt(1, id_patient); 
+        pstmt1.setString(2, date_ordonnance); 
+        pstmt1.setString(3, ordonnace);
     }
     
     
-	
+	//ajouter une consultation
+    public void ajouterConsultation(String date,int id_consultation, int id_patient,String ordonnance) throws SQLException{
+        string sql2 = "INSERT INTO consultation(date,id_consultation,id_patient,ordonnance) VALUES (?, ?, ?, ?)"
+        PreparedStatement pstmt2 = con.prepareStatement(sql2);
+        pstmt2.setString(1,date);
+        pstmt2.setInt(2,id_consulatation);
+        pstmt2.setInt(3,id_patient);
+        pstmt2.setString(4,ordonnance);
+    }
+
 }
