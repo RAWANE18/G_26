b2d1ebfa21eabd21abf2b362c7776e52c6c84868
lolitt-nels <lolittanelsy@gmail.com>
2024-04-27 10:27:40 +0100

    dependencies check

--- main/src/main/java/cabinet_medical/main/MainApplication.java
+++ main/src/main/java/cabinet_medical/main/MainApplication.java
@@ -2,7 +2,8 @@ package cabinet_medical.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@EnableJpaRepositories
 @SpringBootApplication
 public class MainApplication {
 
