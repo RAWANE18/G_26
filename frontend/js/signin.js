0c8f0483b9d7fb40434b709bdb74f0234c06ee57
RAWANE18 <rawanezineb689@gmail.com>
2024-04-29 20:45:32 +0100

    commit tables

--- frontend/js/signin.js
+++ frontend/js/signin.js
@@ -7,6 +7,8 @@ document.getElementById("btn").addEventListener("click", function (event) {
 
   if (username === "doctor" && password === "1234") {
     window.location.href = "Plistdoctor.html";
+  } else if (username === "secretaire" && password === "1234") {
+    window.location.href = "secretaire.html"; // Redirige vers index.html
   } else {
     signinLink.textContent = "Nom d'utilisateur ou mot de passe incorrect.";
     signinLink.style.color = "red";
