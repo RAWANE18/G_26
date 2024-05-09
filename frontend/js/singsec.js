59343279e390aa648db9e383fe29ea846dd79d3a
RAWANE18 <rawanezineb689@gmail.com>
2024-05-09 23:02:19 +0100

    committed

--- frontend/js/singsec.js
+++ frontend/js/singsec.js
@@ -5,8 +5,8 @@ document.getElementById("btn").addEventListener("click", function (event) {
   var password = document.getElementById("passeword").value;
   var signinLink = document.querySelector(".signin-link");
 
-  if (username === "doctor" && password === "1234") {
-    window.location.href = "Plistdoctor.html";
+  if (username === "secretaire" && password === "5678") {
+    window.location.href = "secretaire.html"; // Redirige vers index.html
   } else {
     signinLink.textContent = "Nom d'utilisateur ou mot de passe incorrect.";
     signinLink.style.color = "red";
